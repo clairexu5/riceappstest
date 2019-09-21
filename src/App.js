e2971890a4152dc4899d630e7f9c785c046d0d57
Claire Xu <claire02.xu@gmail.com>
2019-09-21 14:42:50 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import dog from './dogpicture.jpeg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [component, setComponent] = useState(<h1>Team Family Wiess</h1>);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <h1>Claire Xu</h1>
+          <img src={dog} className="App-logo" alt="dogpic" />
+          <a
+            className="App-link"
+            href="http://www.teamwiess.com/"
+          >
+            Wiess is pretty cool!
+          </a>
+        </header>
+      </div>
+      <div>
+        {component}
+        <button onClick={() => setComponent(<h4>Yuh</h4>)}>
+            Click me!
+        </button>
+      </div>
     </div>
-  );
-}
+  )}    
 
 export default App;
